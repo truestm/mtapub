1dda8156a1b663125724deaefef8c2b31103afe8
Anton Bruhanov <nejaleika@gmail.com>
2018-08-06 11:59:23 +0300

    more ada

--- async.lua
+++ async.lua
@@ -21,7 +21,7 @@ function await(func, ...)
 	return task
 end
 
-function asyncComplete(taskid, ...)
+function asyncComplete( taskid, ... )
 	local task = tasks[taskid]
 	task.result = {...}
 	task.complete = true
@@ -38,7 +38,9 @@ function asyncResult(task)
 		while not task.complete do
 			coroutine.yield()
 		end
-		return unpack(task.result)
+		if task.result then 
+			return unpack(task.result) 
+		end
 	else
 		while threads[coroutine.running()] do
 			coroutine.yield()
@@ -46,33 +48,96 @@ function asyncResult(task)
 	end
 end
 
-function asyncTimer( taskid, callback, time, iteration, ... )
-	setTimer( callback, time, iteration, taskid, ... )
+function asyncIsComplete(task)
+	return task.complete
 end
 
-function asyncDbQuery( taskid, connection, sql )
-	dbQuery( asyncDbQueryComplete, { taskid }, connection, sql )
+function asyncWaitAny(...)
+	local count = select("#",...)
+	while true do
+		for i = 1,count do 
+			local task = select(i,...)
+			if task.complete then return task end
+		end
+		coroutine.yield()
+	end
+end
+
+function asyncWaitAll(...)
+	local complete
+	repeat
+		complete = true
+		for i=1,select("#",...) do 
+			local task = select(i,...)
+			if not task.complete then
+				complete = false
+				break
+			end
+		end
+		coroutine.yield()
+	until complete
+end
+
+function asyncSleepStart( taskid, time )
+	setTimer( asyncComplete, time, 1, taskid )
+end
+
+function asyncSleep( time )
+	return await(asyncSleepStart, time)
 end
 
 function asyncDbQueryComplete( handle, taskid )
 	asyncComplete( taskid, dbPoll( handle, 0 ) )
 end
+	
+local function asyncDbQueryStart( taskid, connection, sql )
+	dbQuery( asyncDbQueryComplete, { taskid }, connection, sql )
+end
 
---[[
--- EXAMPLE:
+function asyncDbQuery( connection, sql )
+	return await( asyncDbQueryStart, connection, sql )
+end
+
+local function asyncWaitEventStart( taskid, event, element )
+	local handler
+	handler = function(...)
+		removeEventHandler( event, element, handler )
+		asyncComplete( taskid, client, source, ... )
+	end
+	addEventHandler( event, element, handler )
+end
+
+function asyncWaitEvent( event, element )
+	return await( asyncWaitEventStart, event, element )
+end
 
 addEventHandler("onResourceStart", resourceRoot, async(function(...)
-	outputDebugString("async start "..tostring(getTickCount()))	
+	outputDebugString("async start")	
 	
-	local connection = dbConnect("sqlite", "data/game.dynamic.sqlite")
+	local connection = dbConnect("sqlite", "testdb.sqlite")
 	
-	local r0 = await(asyncDbQuery, connection, "SELECT 1")
+	local task0 = asyncDbQuery(connection, "SELECT 1")	
+	local task1 = asyncSleep(10000)
+	local task2 = asyncSleep(10000)
+	local task3 = asyncWaitEvent("onResourceStop", resourceRoot)
 	
-	local r1 = await(asyncTimer, function(taskid) asyncComplete(taskid, getTickCount()) end, 10000, 1)
-	local r2 = await(asyncTimer, function(taskid) asyncComplete(taskid, getTickCount()) end, 10000, 1)
+	outputDebugString("query result "..tostring(asyncResult(task0)[1]["1"]))
 	
-	iprint(asyncResult(r0))
+	asyncResult(task1)
+	outputDebugString("sleep 1 end ")
 	
-	outputDebugString("async end "..tostring(asyncResult(r1))..","..tostring(asyncResult(r2)))
+	asyncResult(task2)
+	outputDebugString("sleep 2 end ")
+	
+	while not asyncIsComplete( task3 ) do
+		
+		local task4 = asyncWaitEvent("onPlayerJoin", root)
+		
+		if asyncWaitAny( task3, task4 ) == task4 then
+			local client, source = asyncResult( task4 )
+			outputChatBox( "hello "..tostring(source), source )
+		end
+	end
+	
+	outputDebugString("async end")
 end))
-]]
